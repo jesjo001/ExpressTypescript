bff957c2d641f7b4d69703a839b9a79cbd3421e0
jesjo001 <automationlounge@gmail.com>
2021-12-20 09:16:03 +0100

    username verification

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
 import config from "config";
-import log from './logger' //logger doesnt block I/O like console.log does
-import connect from './db/connect';
-import  deserializeUser  from './middleware/validation/deserializeUser'
+import log from "./logger"; //logger doesnt block I/O like console.log does
+import connect from "./db/connect";
+import deserializeUser from "./middleware/validation/deserializeUser";
 dotenv.config();
 
-
-import Route from './routes/routes'
+import Route from "./routes/routes";
 // const PORT = process.env.PORT as number
 // const HOST = process.env.HOST as string
 
 const HOST = config.get("host") as string;
 const PORT = config.get("port") as number;
-
 
 const app = express();
 app.use(deserializeUser);
@@ -23,13 +21,11 @@ app.use(deserializeUser);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/', Route)
-app.get('/', (req, res) => res.status(200).send("Welcome to Insta Feeds"))
-app.get('*', (req, res) => res.status(404).send("Page not found"))
+app.use("/api/v1/", Route);
+app.get("/", (req, res) => res.status(200).send("Welcome to Insta Feeds"));
+app.get("*", (req, res) => res.status(404).send("Page not found"));
 
-app.listen(PORT, HOST, ()=> {
-    log.info(`Server listening at http://${HOST}:${PORT}`)
-    connect();
+app.listen(PORT, HOST, () => {
+  log.info(`Server listening at http://${HOST}:${PORT}/api/v1`);
+  connect();
 });
-
- 
